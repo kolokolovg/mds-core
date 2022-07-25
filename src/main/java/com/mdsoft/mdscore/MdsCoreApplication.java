a3623e012f32b2bbd626a02c4c8d9dcf2e8bde98
GKolokolov <gleb.kolokolov@softline.com>
2022-07-25 18:06:45 +0300

    Initial commit

---
+++ src/main/java/com/mdsoft/mdscore/MdsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.mdsoft.mdscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdsCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mdsoft/mdscore/MdsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdsoft.mdscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdsCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
